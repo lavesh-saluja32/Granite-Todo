ede8732c7383f5d0288989e649722395a08aa6cb
admin <admin@admins-MacBook-Pro.local>
2025-02-25 21:57:38 +0530

    Added constraint to make title not nullable

--- app/models/task.rb
+++ app/models/task.rb
@@ -3,9 +3,16 @@
 # Table name: tasks
 #
 #  id         :integer          not null, primary key
-#  title      :text
+#  title      :text             not null
 #  created_at :datetime         not null
 #  updated_at :datetime         not null
 #
 class Task < ApplicationRecord
+  MAX_TITLE_LENGTH = 125
+  VALID_TITLE_REGEX = /\A.*[a-zA-Z0-9].*\z/i
+
+  validates :title,
+    presence: true,
+    length: { maximum: MAX_TITLE_LENGTH },
+    format: { with: VALID_TITLE_REGEX }
 end
